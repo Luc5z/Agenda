fc5cf43ae0afddfe22a29b54d882e0c1c546b15f
Dival Lucas <lusca404@gmail.com>
2024-09-25 21:12:41 -0300

    adding models, templates and views

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=255)
+    phone = models.CharField(max_length=20)
+
+    created_date = models.DateTimeField(default=timezone.now)
