ad1bed233610b921397056823db7efd7a560c2dc
Dival Lucas <lusca404@gmail.com>
2024-10-01 14:18:17 -0300

    admin configuration

--- contact/admin.py
+++ contact/admin.py
@@ -3,7 +3,9 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone', 'created_date')
     search_fields = ('first_name', 'last_name', 'email', 'phone')
     list_filter = ('created_date',)
     date_hierarchy = 'created_date'
     ordering = ('-created_date',)
+    list_per_page = 10
