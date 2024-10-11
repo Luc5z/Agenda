125bbae68bb703a2883d89240f85d89a16be0de5
Dival Lucas <lusca404@gmail.com>
2024-10-11 18:40:35 -0300

    configuring staticfiles

--- contact/models.py
+++ contact/models.py
@@ -7,8 +7,9 @@ class Contact(models.Model):
     email = models.EmailField(max_length=255)
     phone = models.CharField(max_length=20)
     description = models.TextField(blank=True)
-
     created_date = models.DateTimeField(default=timezone.now)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, null=True, upload_to='pictures/%Y/%m/')
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
