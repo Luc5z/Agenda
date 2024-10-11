98d66f4451fcf3c53eb71844cec2831e96d05232
Dival Lucas <lusca404@gmail.com>
2024-10-11 19:30:52 -0300

    adding owner

--- contact/models.py
+++ contact/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
         return self.name
 
 class Contact(models.Model):
+    class Meta:
+        verbose_name = 'Contato'
+        verbose_name_plural = 'Contatos'
+
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=255)
@@ -17,6 +27,7 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, null=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
