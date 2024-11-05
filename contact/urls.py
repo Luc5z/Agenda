78458eef1494d3b61d539ae15da72cd4a5cd6a33
Dival Lucas <lusca404@gmail.com>
2024-11-05 15:39:31 -0300

    ...

--- contact/urls.py
+++ contact/urls.py
@@ -4,6 +4,6 @@ from contact.views import ContactListView, ContactDetailView
 app_name = 'contact'
 
 urlpatterns = [
-    path('', ContactListView.as_view(), name='contact_list'),
+    path('', ContactListView.as_view(), name='index'),
     path('contact/<int:pk>/', ContactDetailView.as_view(), name='contact_detail'),
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
 from django.views.generic import ListView, DetailView
 
@@ -16,3 +16,11 @@ class ContactDetailView(DetailView):
     model = Contact
     template_name = 'contact/contact.html'
     context_object_name = 'contact'
+
+    def get_object(self):
+        contact_id = self.kwargs.get('pk')
+        single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+        self.site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+        return single_contact
+
+
